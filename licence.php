e2f77558263709d18789794caf5d0e6599cab935
01emastaff <matsufuji@emanet.jp>
2025-07-31 18:29:34 +0900

    table追加

--- licence.php
+++ licence.php
@@ -501,8 +501,36 @@
                                     ※安心パックを利用せずに卒業された場合でも返金はございません。<br>
                                     ※２回目以降の各検定料および仮免学科試験料は別途必要になります。<br>
                                 </p>
+                                <div class="safetyWrap"　style="display:none;">
+                                    <div class="safetyBox">
+                                        <div class="boxTable">
+                                        <table>
+                                            <tr>
+                                                <th></th>
+                                                <th>パック料金</th>
+                                                <th>保証時限数</th>
+                                            </tr>
+                                            <tr>
+                                                <th>安心3</th>
+                                                <td>8,530円</td>
+                                                <td>3時限</td>
+                                            </tr>
+                                            <tr>
+                                                <th>安心5</th>
+                                                <td>14,300円</td>
+                                                <td>5時限</span></td>
+                                            </tr>
+                                            <tr>
+                                                <th>無制限</th>
+                                                <td>28,600円</td>
+                                                <td>無制限</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                                </div>
+                            </div>
                         <div class="wrapBasicPlan planSpeed" id="plan-speed">
                             <h3 class="titleItemContent">スピードプラン</h3>
                             <div class="contentItem">
