f6f97c71bd726462251a2271ecb09732949b98d5
syu5687 <mk@emanet.jp>
2025-08-05 10:38:16 +0900

    Update header.php

--- header.php
+++ header.php
@@ -40,11 +40,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="assets/css/common.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/common.css">
     
-    <link rel="stylesheet" type="text/css" href="assets/css/<?php echo $filename; ?>.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/<?php echo $filename; ?>.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script type="text/javascript" src="assets/js/script.js"></script>
+    <script type="text/javascript" src="/assets/js/script.js"></script>
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
