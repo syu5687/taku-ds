13cd9bc8e48633b88a3e19d5ae50fec1220bfffe
01emastaff <matsufuji@emanet.jp>
2025-10-07 16:36:55 +0900

    Update header.php

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/assets/css/common.css">
     
-    <link rel="stylesheet" type="text/css" href="/assets/css/<?php echo $filename; ?>.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/<?php echo $filename; ?>.css?<?php echo date('Ymd-His'); ?>">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script type="text/javascript" src="/assets/js/script.js"></script>
     <!-- Google Tag Manager -->
