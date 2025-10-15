469300ce291c3328a827c77ff2f22a7421f4124e
01emastaff <matsufuji@emanet.jp>
2025-10-15 13:14:13 +0900

    Update header.php

--- header.php
+++ header.php
@@ -49,7 +49,7 @@
     
     <link rel="stylesheet" type="text/css" href="/assets/css/<?php echo $filename; ?>.css?<?php echo date('Ymd-His'); ?>">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script type="text/javascript" src="/assets/js/script.js"></script>
+    <script type="text/javascript" src="/assets/js/script.js?<?php echo date('Ymd-His'); ?>"></script>
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
