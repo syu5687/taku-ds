655f19298602e35f59ac51db2237d0ac8af8656a
01emastaff <matsufuji@emanet.jp>
2025-12-22 15:34:36 +0900

    # 002:feature: CSS調整

--- licence.php
+++ licence.php
@@ -565,8 +565,8 @@
                                             </p>
                                         </li>
                                     </ul>
-                                    <p>※MTをご希望の方、各プランについては事前にお問い合わせ下さい。<br>
-                                        TEL：<a href="tel:0952-76-2050">0952-76-2050</a><br>
+                                    <p class="planTelMail">※MTをご希望の方、各プランについては事前にお問い合わせ下さい。<br>
+                                        TEL:<a href="tel:0952-76-2050">0952-76-2050</a><br>
                                         mail:<a href="mailto:info@taku-ds.jp">info@taku-ds.jp</a></p>
                                 </div>
                                 <div class="licenceBtn">
